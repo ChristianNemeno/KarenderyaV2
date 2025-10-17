e1c1c26aed96d7711b97d20035582fa91a25e172
christian <christiannemeno80@gmail.com>
2025-10-17 16:07:59 +0800

    sad

---
+++ laarnis/src/components/button.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+function Button({ children, onClick }: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
