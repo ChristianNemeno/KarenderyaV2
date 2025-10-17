ebaed81cab52f9ee9a9638e43428af46310c3727
christian <christiannemeno80@gmail.com>
2025-10-17 20:44:59 +0800

    working

--- laarnis/src/components/button.tsx
+++ laarnis/src/components/button.tsx
@@ -9,7 +9,7 @@ function Button({ children, onClick }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+      className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-red-600"
     >
       {children}
     </button>
