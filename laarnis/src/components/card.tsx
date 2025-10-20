178d7a5a8039dba4da452df12e527780a2a94811
christian <christiannemeno80@gmail.com>
2025-10-20 16:09:53 +0800

    improve card

--- laarnis/src/components/card.tsx
+++ laarnis/src/components/card.tsx
@@ -7,10 +7,15 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({title, content}) => {
     return (
-        <div className = "flex flex-col"> 
+        <div className = "flex flex-col w-80 max-w-sm mx-auto rounded-lg overflow-hidden"> 
+            
             <div className="p-4 bg-blue-300">{title}</div>
+            
             <div className="p-4 bg-green-300">{content}</div>
+            
         </div>
+
+        
     )
 }
 
