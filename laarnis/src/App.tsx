c27e8f40e6ae791241dad0a091c34364e2cfaea9
christian <christiannemeno80@gmail.com>
2025-10-21 22:38:55 +0800

    nav bar unstyled

--- laarnis/src/App.tsx
+++ laarnis/src/App.tsx
@@ -2,11 +2,25 @@
 import './App.css'
 
 import Card from './components/card';
+import Navbar from './components/navbar';
 
 function App() {
 
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Contact", href: "/contact" }
+  ];
+
   return (
     <>
+    <Navbar
+      logo="Karenderya"
+      links={navLinks}
+    />
+
+    
+
      <Card 
       title = "I am"
       content="Very sad"
--- laarnis/src/components/navbar.tsx
+++ laarnis/src/components/navbar.tsx
@@ -6,3 +6,27 @@ type NavbarProps = {
     links?: { name: string; href: string }[];
 };
 
+
+const Navbar: React.FC<NavbarProps> = ({ logo = "MyApp", links = []}) => {
+    return (
+        <nav>
+            <div>
+                <a href="#">{logo}</a>
+            </div>
+            
+            <div>
+                {links.map((link, index) => (
+                    <a  key = {index}
+                        href = {link.href}
+                    >
+                        {link.name}
+                    </a>
+                ))}
+                
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
+
