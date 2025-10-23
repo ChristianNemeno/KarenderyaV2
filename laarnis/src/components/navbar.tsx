a457112a1ed673aa916cca65f5ace6881e6d8eb5
christian <christiannemeno80@gmail.com>
2025-10-23 19:33:24 +0800

    flex navbar

--- laarnis/src/components/navbar.tsx
+++ laarnis/src/components/navbar.tsx
@@ -1,32 +1,36 @@
 import React from 'react';
 
-
 type NavbarProps = {
-    logo?: string;
-    links?: { name: string; href: string }[];
+  logo?: string;
+  links?: { name: string; href: string }[];
 };
 
+const Navbar: React.FC<NavbarProps> = ({ logo = "MyApp", links = [] }) => {
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <div className="flex items-center justify-between max-w-6xl mx-auto">
+        {/* Logo */}
+        <div className="text-xl font-bold">
+          <a href="/" className="hover:text-blue-200">
+            {logo}
+          </a>
+        </div>
 
-const Navbar: React.FC<NavbarProps> = ({ logo = "MyApp", links = []}) => {
-    return (
-        <nav className="bg-blue-500 text-white p-4">
-            <div>
-                <a href="#">{logo}</a>
-            </div>
-            
-            <div>
-                {links.map((link, index) => (
-                    <a  key = {index}
-                        href = {link.href}
-                    >
-                        {link.name}
-                    </a>
-                ))}
-
-            </div>
-        </nav>
-    );
+        {/* Navigation Links */}
+        <div className="flex space-x-6">
+          {links.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              className="hover:text-blue-200 transition-colors"
+            >
+              {link.name}
+            </a>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
-
