73381b2f56ff69acb9486770c3b937f72952fe88
christian <christiannemeno80@gmail.com>
2025-10-29 18:49:34 +0800

    card changes

--- laarnis/src/components/card.tsx
+++ laarnis/src/components/card.tsx
@@ -3,11 +3,18 @@ import React from 'react';
 type CardProps = {
     title: string;
     content: string;
+    className?: string;
 };
 
-const Card: React.FC<CardProps> = ({title, content}) => {
+const Card: React.FC<CardProps> = ({ 
+    title, 
+    content, 
+    className 
+}) => {
+    
+    
     return (
-        <div className = "flex flex-col w-80 max-w-sm mx-auto rounded-lg overflow-hidden"> 
+        <div className = {`flex flex-col w-80 max-w-sm mx-auto rounded-lg overflow-hidden ${className}`}> 
             
             <div className="p-4 bg-blue-300">{title}</div>
             
